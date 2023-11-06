84a7bb880eae8a0fcbbf5edf04a26d11abe02fa5
Monerat <gmonerat@gmail.com>
2023-11-06 14:49:37 -0300

    hotfix

--- Tech Clothes/src/pages/Login.jsx
+++ Tech Clothes/src/pages/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault()
-    setUsuario(user.filter((user) => user.email === email && user.senha === senha))
+    setUsuario(user.filter((user) => user.email == email && user.senha == senha))
     handleAuth()
   }
 
